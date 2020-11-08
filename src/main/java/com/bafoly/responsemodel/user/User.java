c918d0b05113844ada91ff53674c35c0d99a4408
Basar Buyukkahraman <basarbk@yahoo.com>
2020-11-08 19:18:50 +0300

    Revert jsonview

--- src/main/java/com/bafoly/responsemodel/user/User.java
+++ src/main/java/com/bafoly/responsemodel/user/User.java
@@ -5,8 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonView;
-
 import lombok.Data;
 
 @Data
@@ -15,14 +13,11 @@ public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @JsonView(View.Admin.class)
   private long id;
 
 
-  @JsonView(View.Base.class)
   private String username;
   
-  @JsonView(View.Base.class)
   private String email;
 
   private String password;
--- src/main/java/com/bafoly/responsemodel/user/View.java
+++
@@ -1,9 +0,0 @@
-package com.bafoly.responsemodel.user;
-
-public interface View {
-
-  public class Base {}
-
-  public class Admin extends Base {}
-  
-}
