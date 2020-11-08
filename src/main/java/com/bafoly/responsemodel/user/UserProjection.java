f556ab983b917921ec84e5856c60a7b648c923ba
Basar Buyukkahraman <basarbk@yahoo.com>
2020-11-08 19:24:57 +0300

    Show article count in user projection

--- src/main/java/com/bafoly/responsemodel/user/UserProjection.java
+++ src/main/java/com/bafoly/responsemodel/user/UserProjection.java
@@ -1,18 +1,15 @@
 package com.bafoly.responsemodel.user;
 
-import java.util.List;
-
-import com.bafoly.responsemodel.article.ArticleProjection;
-
 import org.springframework.beans.factory.annotation.Value;
 
 public interface UserProjection {
 
-  @Value("#{target.username + ' ' + target.lastname}")
+  @Value("#{target.username + ' ' + target.lastname.toUpperCase()}")
   String getFullName();
   
   String getEmail();
 
-  List<ArticleProjection> getArticles();
+  @Value("#{target.articles.size()}")
+  int getArticleCount();
   
 }
